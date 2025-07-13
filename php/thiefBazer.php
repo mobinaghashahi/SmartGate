6309323c804ae10e17c4bb4d1e17c750e7b6283b
mobin <mobinaghashahi13@gmail.com>
2025-07-13 18:37:56 +0330

    change something

--- php/thiefBazer.php
+++ php/thiefBazer.php
@@ -1,12 +1,13 @@
 <?php
-$config = require 'config.php';
+require 'config.php';
+
 
 error_reporting(E_ALL);
 ini_set('display_errors', TRUE);
 ini_set('display_startup_errors', TRUE);
 
 
-$botToken = $config['BOT_TOKEN'];
+
 $webSite = "https://api.telegram.org/bot" . $botToken;
 
 $update = file_get_contents("php://input");
@@ -18,80 +19,81 @@ $message = $update["message"]["text"];
 
 
 date_default_timezone_set('Asia/Tehran');
-$today = date("D m j Y G:i:s");
-if($_POST['key']==$config['KEY_WEBSITE']){
-
+$today = date("D m j Y G:i:s"); 
+if($_POST['key']=="Mobin.7060.mobin.ali.$#@!"){
+    
     if($_POST['action']=="ringON"){
         //sendToNotificationUsers("یکی زنگ زد!! ");
         //send Message to Mobin
-        //sendMessage("159354346","یکی زنگ زد!! ");
-        sendPhotoToNotificationUsers($config['IMAGE_SOURCE'],$botToken);
-        //send Message to Sedighe
-        //sendMessage("168232585","یکی زنگ زد!! ");
-        //sendPhoto(168232585, "uploads/1.jpg",$botToken);
-        //send Message to Mahsa
-        //sendMessage("128998617","یکی زنگ زد!! ");
-        //sendPhoto(128998617, "uploads/1.jpg",$botToken);
-        //send Message to Mahdiyeh
-        //sendMessage("126813675","یکی زنگ زد!! ");
-        //sendPhoto(126813675, "uploads/1.jpg",$botToken);
-        //send Message to Moein
-        //sendMessage("259898553","یکی زنگ زد!! ");
-        //sendPhoto(259898553, "uploads/1.jpg",$botToken);
-        //send Message to AliReza
-        //sendMessage("284514919","یکی زنگ زد!! ");
-        //sendPhoto(284514919, "uploads/1.jpg",$botToken);
-        //send Message to Mohadeseh
-        //sendMessage("6505981674","یکی زنگ زد!! ");
-        //sendPhoto(6505981674, "uploads/1.jpg",$botToken);
+	    //sendMessage("159354346","یکی زنگ زد!! ");
+	    sendPhotoToNotificationUsers("uploads/1.jpg",$botToken);
+  	    //send Message to Sedighe
+  	    //sendMessage("168232585","یکی زنگ زد!! ");
+  	    //sendPhoto(168232585, "uploads/1.jpg",$botToken);
+  	    //send Message to Mahsa
+  	    //sendMessage("128998617","یکی زنگ زد!! ");
+  	    //sendPhoto(128998617, "uploads/1.jpg",$botToken);
+  	    //send Message to Mahdiyeh
+  	    //sendMessage("126813675","یکی زنگ زد!! ");
+  	    //sendPhoto(126813675, "uploads/1.jpg",$botToken);
+  	    //send Message to Moein
+  	    //sendMessage("259898553","یکی زنگ زد!! ");
+  	    //sendPhoto(259898553, "uploads/1.jpg",$botToken);
+  	    //send Message to AliReza
+  	    //sendMessage("284514919","یکی زنگ زد!! ");
+  	    //sendPhoto(284514919, "uploads/1.jpg",$botToken);
+  	    //send Message to Mohadeseh
+  	    //sendMessage("6505981674","یکی زنگ زد!! ");
+  	    //sendPhoto(6505981674, "uploads/1.jpg",$botToken);
     }
     else if($_POST['action']=="thifAlert"){
         sendToNotificationUsers("یکی وارد خانه شد!! ");
-        //sendMessageToAll("یکی وارد خانه شد!! ");
+  	    //sendMessageToAll("یکی وارد خانه شد!! ");
     }
     else if($_POST['action']=="passwordChanged"){
         sendToNotificationUsers("رمز عبور درب به: ".$_POST['password']." تغییر کرده است.");
-        //sendMessageToAll("رمز عبور درب به: ".$_POST['password']." تغییر کرده است.");
+  	    //sendMessageToAll("رمز عبور درب به: ".$_POST['password']." تغییر کرده است.");
     }
-    else if($_POST['action']=="wrongPassword"){
-        sendToNotificationUsers("رمز عبور اشتباه وارد شده است => ".$_POST['password']);
-        //sendMessageToAll("رمز عبور اشتباه وارد شده است => ".$_POST['password']);
+  	else if($_POST['action']=="wrongPassword"){
+      	    sendToNotificationUsers("رمز عبور اشتباه وارد شده است => ".$_POST['password']);
+  	    //sendMessageToAll("رمز عبور اشتباه وارد شده است => ".$_POST['password']);
     }
     else if($_POST['action']=="openedDoor"){
-        sendToNotificationUsers("برق اتاق توسط  ".whichUser($_POST['whichUser'])." روشن شد.");
+        sendToNotificationUsers("درب خانه توسط ".whichUser($_POST['whichUser'])." باز شد.");
         //sendMessageToAll("درب خانه توسط  ".whichUser($_POST['whichUser'])." باز شد.");
     }
     else if($_POST['action']=="turnOnLight"){
-        sendToNotificationUsers("برق اتاق توسط  ".whichUser($_POST['whichUser'])." روشن شد.");
+        sendToNotificationUsers("برق اتاق توسط ".whichUser($_POST['whichUser'])." روشن شد.");
         //sendMessageToAll("برق اتاق توسط  ".whichUser($_POST['whichUser'])." روشن شد.");
     }
     else if($_POST['action']=="turnOffLight"){
         //sendMessageToAll("برق اتاق توسط  ".whichUser($_POST['whichUser'])." خاموش شد.");
-        sendToNotificationUsers("برق اتاق توسط  ".whichUser($_POST['whichUser'])." خاموش شد.");
+        sendToNotificationUsers("برق اتاق توسط ".whichUser($_POST['whichUser'])." خاموش شد.");
     }
     else if($_POST['action']=="theDeviseIsReady"){
         sendToNotificationUsers("دستگاه آماده به کار است!! خیالت راحت باشه.");
         //sendMessageToAll("دستگاه آماده به کار است!! خیالت راحت باشه.");
     }
+    else if($_POST['action']=="sshLogin"){
+        sendToNotificationUsers("یکی با آی پی ".$_POST['ip']." به سرور ssh کرد.");
+        //sendMessageToAll("دستگاه آماده به کار است!! خیالت راحت باشه.");
+    }
 }
 
-
+	
 
 
 function sendMessage($chatId, $message)
 {
+    require 'config.php';
     $url = $GLOBALS['webSite'] . "/sendMessage?chat_id=" . $chatId . "&text=" . urlencode($message);
     file_get_contents($url);
 }
 function sendPhotoToNotificationUsers($photoPath,$botToken){
+    require 'config.php';
     $url = "https://api.telegram.org/bot$botToken/sendPhoto";
 
-    $database = [
-        'host' => $config['DB_HOST'],
-        'dbname' => $config['DB_NAME'],
-        'user' => $config['DB_USER'],
-        'pass' => $config['DB_PASSWORD']
-    ];
+    
 
     try {
         $db = new PDO("mysql:host={$database['host']};dbname={$database['dbname']}", $database['user'], $database['pass']);
@@ -105,11 +107,11 @@ function sendPhotoToNotificationUsers($photoPath,$botToken){
     $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     foreach ($rows as $row){
-
+        
         if($row['notification'] ==1){
 
             $ch = curl_init();
-
+            
             $postData = [
                 'chat_id' => $row['telegramID'],
                 'caption' => 'یکی زنگ زد!',
@@ -119,7 +121,7 @@ function sendPhotoToNotificationUsers($photoPath,$botToken){
             curl_setopt($ch, CURLOPT_POST, true);
             curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
+            
             $response = curl_exec($ch);
             curl_close($ch);
         }
@@ -129,7 +131,7 @@ function sendPhotoToNotificationUsers($photoPath,$botToken){
 }
 function sendPhoto($chatId, $photoPath,$botToken)
 {
-
+    require 'config.php';
     $url = "https://api.telegram.org/bot$botToken/sendPhoto";
 
     $postData = [
@@ -150,29 +152,24 @@ function sendPhoto($chatId, $photoPath,$botToken)
     return $response;
 }
 function sendMessageToAll($message){
-
+    require 'config.php';
     //send Message to Mobin
     sendMessage("159354346",$message);
-    //send Message to Sedighe
-    sendMessage("168232585",$message);
-    ////send Message to Mahsa
-    sendMessage("128998617",$message);
-    ////send Message to Mahdiyeh
-    sendMessage("126813675",$message);
-    ////send Message to Moein
-    sendMessage("259898553",$message);
-    ////send Message to AliReza
+  	//send Message to Sedighe
+  	sendMessage("168232585",$message);
+  	////send Message to Mahsa
+  	sendMessage("128998617",$message);
+  	////send Message to Mahdiyeh
+  	sendMessage("126813675",$message);
+  	////send Message to Moein
+  	sendMessage("259898553",$message);
+  	////send Message to AliReza
     sendMessage("284514919",$message);
-    ////send Message to Mohadeseh
-    sendMessage("6505981674",$message);
+  	////send Message to Mohadeseh
+  	sendMessage("6505981674",$message);
 }
 function sendToNotificationUsers($message){
-    $database = [
-        'host' => $config['DB_HOST'],
-        'dbname' => $config['DB_NAME'],
-        'user' => $config['DB_USER'],
-        'pass' => $config['DB_PASSWORD']
-    ];
+    require 'config.php';
 
     try {
         $db = new PDO("mysql:host={$database['host']};dbname={$database['dbname']}", $database['user'], $database['pass']);
@@ -193,53 +190,42 @@ function sendToNotificationUsers($message){
     //sendMessage($chatId,$allUsers);
     //send Message to Mobin
     //sendMessage("159354346",$message);
-    //send Message to Sedighe
-    //sendMessage("168232585",$message);
-    ////send Message to Mahsa
-    //sendMessage("128998617",$message);
-    ////send Message to Mahdiyeh
-    //sendMessage("126813675",$message);
-    ////send Message to Moein
-    //sendMessage("259898553",$message);
-    ////send Message to AliReza
+  	//send Message to Sedighe
+  	//sendMessage("168232585",$message);
+  	////send Message to Mahsa
+  	//sendMessage("128998617",$message);
+  	////send Message to Mahdiyeh
+  	//sendMessage("126813675",$message);
+  	////send Message to Moein
+  	//sendMessage("259898553",$message);
+  	////send Message to AliReza
     //sendMessage("284514919",$message);
-    ////send Message to Mohadeseh
-    //sendMessage("6505981674",$message);
+  	////send Message to Mohadeseh
+  	//sendMessage("6505981674",$message);
 }
 function whichUser($whichUser){
-    $database = [
-        'host' => $config['DB_HOST'],
-        'dbname' => $config['DB_NAME'],
-        'user' => $config['DB_USER'],
-        'pass' => $config['DB_PASSWORD']
-    ];
-
+    require 'config.php';
     try {
         $db = new PDO("mysql:host={$database['host']};dbname={$database['dbname']}", $database['user'], $database['pass']);
     } catch (PDOException $e) {
         die("An error happend, Error: " . $e->getMessage());
     }
 
-    $database = [
-        'host' => $config['DB_HOST'],
-        'dbname' => $config['DB_NAME'],
-        'user' => $config['DB_USER'],
-        'pass' => $config['DB_PASSWORD']
-    ];
-
+    
+    
     try {
         $db = new PDO("mysql:host={$database['host']};dbname={$database['dbname']}", $database['user'], $database['pass']);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $e) {
         die("An error happened: " . $e->getMessage());
     }
-
+    
     $sql = "SELECT * FROM users WHERE telegramID = :telegramID";
     $stmt = $db->prepare($sql);
-    $stmt->bindParam(':telegramID', $whichUser, PDO::PARAM_STR);
+    $stmt->bindParam(':telegramID', $whichUser, PDO::PARAM_STR); 
     $stmt->execute();
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
-
+    
     if ($row) {
         return $row['displayName'];
     } else {
@@ -247,10 +233,12 @@ function whichUser($whichUser){
             return "کلید";
         else if($whichUser=="keyPad")
             return "کیبورد";
+        else if($whichUser=="computer")
+            return "سیستم";
     }
 
-
-
+    
+    
     //if($whichUser==159354346)
     //    return "مبین";
     //else if($whichUser==168232585)
@@ -271,6 +259,6 @@ function whichUser($whichUser){
     //    return "کلید";
     //else if($whichUser=="keyPad")
     //    return "کیبورد";
-
+        
 }
 
